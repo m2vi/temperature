2be029c475bd90f6841ff46fca4560675d25ba2c
m2v <69117810+iuvoJS@users.noreply.github.com>
2021-09-26 10:13:54 +0200

    heizung

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,27 +1,28 @@
 import { Buttput, Digiput, Mainput, Wrapput } from '../components/styles';
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { Units, temperature } from '../utils/temperature';
 import { capitalize, copy } from '../utils/text';
 import { IoPinOutline } from 'react-icons/io5';
 import Link from 'next/link';
 
 const Main = ({ unit }) => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(temperature(0).from(Units[unit]).to(Units.kelvin));
 
   const mainUnit = Object.keys(Units).find((u) => u === unit);
   const otherUnits = Object.keys(Units).filter((u) => u !== unit);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const v = parseInt(e.currentTarget.value ? e.currentTarget.value : '0');
+    const k = temperature(v).from(Units[mainUnit]).to(Units.kelvin);
+
+    setValue(k);
+  };
 
   return (
     <>
       <Wrapput className='mb-5'>
         <Digiput title='Base'>{Units[mainUnit]}</Digiput>
-        <Mainput
-          title='Number System'
-          onChange={(e) => setValue(parseInt(e.currentTarget.value ? e.currentTarget.value : '0'))}
-          placeholder={capitalize(mainUnit)}
-          type='number'
-          defaultValue='0'
-        />
+        <Mainput title='Number System' onChange={handleChange} placeholder={capitalize(mainUnit)} type='number' defaultValue='0' />
         <Buttput style={{ pointerEvents: 'none' }} />
       </Wrapput>
       {otherUnits.map((unit) => {
@@ -32,7 +33,7 @@ const Main = ({ unit }) => {
               title='Number System'
               placeholder={capitalize(unit)}
               readOnly={true}
-              value={temperature(value).from(Units.kelvin).to(Units[unit])}
+              value={Math.round((temperature(value).from(Units.kelvin).to(Units[unit]) + Number.EPSILON) * 100) / 100}
               type='number'
               onClick={(e) => copy(e.currentTarget.value)}
             />
--- utils/temperature.ts
+++ utils/temperature.ts
@@ -10,7 +10,7 @@ class From {
   from(n: Units) {
     switch (n) {
       case Units.fahrenheit:
-        return new To(((this.input - 32) * 5) / 9);
+        return new To((this.input - 32) / 1.8 + 273.15);
       case Units.celsius:
         return new To(this.input + 273.15);
       case Units.kelvin:
@@ -25,11 +25,11 @@ class To {
   to(n: Units) {
     switch (n) {
       case Units.fahrenheit:
-        return (((this.kelvin - 273.15) * 9) / 5 + 32).toFixed(2);
+        return (this.kelvin - 273.15) * 1.8 + 32;
       case Units.celsius:
-        return (this.kelvin - 273.15).toFixed(2);
+        return this.kelvin - 273.15;
       case Units.kelvin:
-        return (this.kelvin - 273.15).toFixed(2);
+        return this.kelvin;
     }
   }
 }
